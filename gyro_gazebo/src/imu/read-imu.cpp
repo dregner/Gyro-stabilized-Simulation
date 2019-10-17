503d59f0db4d836effa75caa086c1fe199e4f319
daniel <daniel_regner@hotmail.com>
2019-10-16 21:18:43 -0300

    adding camera to not open GUI

--- gyro_gazebo/src/imu/read-imu.cpp
+++ gyro_gazebo/src/imu/read-imu.cpp
@@ -1,6 +1,52 @@
 //
 // Created by daniel on 09/10/2019.
 //
-#include "ros/ros.h"
 
-int main(int argc, char **argv) {}
+#include "ros/ros.h"
+#include <ros/node_handle.h>
+#include <sensor_msgs/Imu.h>
+#include <iostream>
+#include <fstream>
+#include <std_msgs/Float64.h>
+#include <ignition/math2/ignition/math/Quaternion.hh>
+
+
+double q_x, q_y, q_z, q_w, roll, pitch, yaw;
+double value;
+double RadToDeg = 180 / M_PI;
+
+
+void read_imu(const sensor_msgs::Imu::ConstPtr &msg) {
+
+    ignition::math::Quaterniond rpy;
+
+    value = msg->header.seq;
+
+    rpy.Set(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
+
+    roll = rpy.Roll();
+    roll *= RadToDeg;
+
+    pitch = rpy.Pitch();
+    pitch *= RadToDeg;
+
+    yaw = rpy.Yaw();
+    yaw *= RadToDeg;
+
+    ROS_INFO("Seq: [%d]", msg->header.seq);
+    ROS_INFO("Orientation -> r: [%f], p: [%f], y: [%f]", roll, pitch, yaw);
+}
+
+
+int main(int argc, char **argv) {
+
+
+    ros::init(argc, argv, "imu_listener");
+
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub_imu = nh.subscribe("/imu_base", 10, read_imu);
+    while (ros::ok()) {
+        ros::spinOnce();
+    }
+}
